7df3d543b215f4cf311e42a0ef5cdfba464aaa0e
Asif-ECE <nishatece13@gmail.com>
2022-01-02 22:48:38 +0600

    Create Entities

---
+++ Entities/Item.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace notes.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; }
+        public String Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
